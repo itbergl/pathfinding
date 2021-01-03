fd020e395a32ca080b784d0cd401252ac204c66f
Isaac Bergl <& d:/OneDrive/git/Poker/.venv/Scripts/python.exe d:/OneDrive/git/Poker/main.pyIsaac Bergl>
2021-01-03 21:50:25 +0800

    A* pathfinding

--- GUI.py
+++ GUI.py
@@ -1,9 +1,13 @@
+from os import path
 from numpy.lib.function_base import select
 import pygame
 from pygame.mixer import pause
 import os
 import numpy as np
 import time
+import math
+from pathfinding import pathfinding as pf
+import sys
 
 #hide default pygame message in console
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
@@ -11,84 +15,136 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 pygame.init()
 
 #Screen
-cell_size = 10
-ncells = 100
+cell_size = 30
+ncells = 30
 line_colour = (200,200,200)
 screen = pygame.display.set_mode((ncells*cell_size,ncells*cell_size))
 
-#board
-board = []
-for r in range(ncells):
-    row = []
-    for c in range(ncells):
-        row.append(True)
-    board.append(row)
+#pathfinding class init
+pathfinding = pf(ncells, (10,-10), (-1,-1))
+
+#facade board
+board_copy = np.copy(pathfinding.board)
+
+# final path
+final_path = []
 
 def drawCell(xcord, ycord, colour):
     #print box
     square = pygame.Rect((xcord*cell_size, ycord*cell_size, cell_size, cell_size)) 
     pygame.draw.rect(screen,colour,square)
 
-def drawBoard(b):
+def drawBoard():
     screen.fill((255,255,255))
     for r in range(ncells):
         
-        for c in range(ncells): 
-            if b[r][c]:
-                colour = (255,255,255) 
-            else:
-                colour = (0,0,0) 
-            drawCell(r,c, colour)
+        for c in range(ncells):           
+                 
+            #   PROBING
+            if pathfinding.heuristic[r][c] != sys.maxsize:
+                drawCell(r,c, (15,210,185))
+                
+            #   SEARCHED
+            if (r,c) in pathfinding.explored:
+                drawCell(r,c, (13,179,154))                    
+            #   WALL
+            if not board_copy[r][c]:
+                drawCell(r,c, (0,0,0)) 
+            #   DESTINATION
+            if (r,c)==pathfinding.dest:
+                drawCell(r,c, (255,0,0))
+            #   START
+            if (r,c)==pathfinding.origin:
+                drawCell(r,c, (0,0,255)) 
+            #   FINAL PATH
+            if (r,c) in final_path:
+                print(final_path)
+                drawCell(r,c, (0,255,0)) 
 
     for r in range(100):
         pygame.draw.line(screen,line_colour,(r*cell_size, 0), (r*cell_size, ncells*cell_size),1)
     for c in range(100):      
         pygame.draw.line(screen,line_colour,(0, c*cell_size), (ncells*cell_size, c*cell_size),1)
 
-def selectCell(pos, fakeboard, draw):
+def selectCell(prev, pos, draw):
     xpos = pos[0]
     ypos = pos[1]
 
-    if board[xpos][ypos] == draw:
-        fakeboard[xpos][ypos] = not draw
+    if board_copy[xpos][ypos] == draw:
+        board_copy[xpos][ypos] = not draw
+        
+        #make it so fast mouse doesn't break
+
+def pathFind():
+    global RUNNING
+    SIMULATING = True
+    FINDING = True
+    node = pathfinding.dest
+    global final_path
+
+    while RUNNING and SIMULATING:
+
+        for event in pygame.event.get():
+            #close game
+            if event.type == pygame.QUIT:
+                RUNNING = False
+
+        if FINDING:
+            FINDING = pathfinding.nextStep()
+        else:
+            node = pathfinding.addToPath(final_path, node)
+            if node == pathfinding.origin:
+                SIMULATING = False
+
+        drawBoard()                
+        pygame.display.update()
+
+
 
 RUNNING = True
 SELECTING = False
 selectedCell = (-1,-1)
-board_copy = np.copy(board)
+
 draw = True
-while RUNNING:
-    
+while RUNNING:    
 
     for event in pygame.event.get():
         #close game
         if event.type == pygame.QUIT:
             RUNNING = False
         if event.type == pygame.MOUSEBUTTONUP :
-            if event.button == 1:
+            if event.button == 2:
                 SELECTING = False
-                board = board_copy
+                pathfinding.board = board_copy
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
+            if event.button == 2:
                 SELECTING = True
-                board_copy = np.copy(board)
+                board_copy = np.copy(pathfinding.board)
+            elif event.button == 3:
+                mouse_pos = pygame.mouse.get_pos()
+                selectedCell = (int(mouse_pos[0]/cell_size), int(mouse_pos[1]/cell_size))
+                pathfinding.dest = selectedCell
+            elif event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                selectedCell = (int(mouse_pos[0]/cell_size), int(mouse_pos[1]/cell_size))
+                pathfinding.addOrigin(selectedCell)
+                
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LCTRL:
                 draw = False
+            if event.key == pygame.K_SPACE:
+                pathFind()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LCTRL:
                 draw = True
+        
             
 
     if SELECTING:
+        prev_selected_cell = selectedCell
         mouse_pos = pygame.mouse.get_pos()
-        selected_cell = (int(mouse_pos[0]/cell_size), int(mouse_pos[1]/cell_size))
-        selectCell(selected_cell, board_copy, draw)
+        selectedCell = (int(mouse_pos[0]/cell_size), int(mouse_pos[1]/cell_size))
+        selectCell(prev_selected_cell, selectedCell, draw)
     
-    drawBoard(board_copy)
-   
-    
-    
-
-        
+    drawBoard()  
     pygame.display.update()
--- pathfinding.py
+++ pathfinding.py
@@ -1,2 +1,105 @@
+import math
+import numpy as np
+import sys
 
+class pathfinding:
+
+    # data structures
+    dist_from_goal = []
+    dist_from_start = []
+    heuristic = []
+    board = []
+    explored = []
+    inprogress = []
+    node_from = []
+
+    # nodes
+    probe = (-1,-1)
+    dest = (-1,-1)
+    origin = (-1,-1)
+
+    def __init__(self, ncells, dest, origin):
+        self.dest = dest
+        self.origin = origin
+        self.probe = origin
+    
+        for r in range(ncells+1):
+            bool = []
+            integer = []
+            infinity = []  
+            node = []
+            for c in range(ncells+1):
+                bool.append(True)   
+                integer.append(0)
+                infinity.append(sys.maxsize)
+                node.append((-1,-1))    
+            self.board.append(bool)
+            self.dist_from_goal.append(integer)
+            self.dist_from_start.append(integer)
+            self.heuristic.append(infinity)      
+            self.node_from.append(node)    
+    
+    def selectPromising(self):
+        MIN = sys.maxsize
+        best = self.origin
+        for (r,c) in self.inprogress:
+            
+            if self.heuristic[r][c] < MIN:
+                MIN = self.heuristic[r][c]
+                best = (r,c)
+        self.probe = best
+        return best
+    
+    def updateNeighbours(self):
+        direcitons = [(-1,0), (1,0), (0,-1), (0, 1)]
+
+        nodex = self.probe[0]
+        nodey = self.probe[1]
+
+        destx = self.dest[0]
+        desty = self.dest[1]
+
+        for (v,h) in direcitons:          
+            self.dist_from_goal[v+nodex][h+nodey] = math.hypot(nodex+v-destx, nodey+h-desty)
+            dist_to = self.dist_from_start[nodex][nodey] + 1
+            potential = dist_to + self.dist_from_goal[v+nodex][h+nodey]
+
+            if  (v+nodex, h+nodey) not in self.explored and potential < self.heuristic[v+nodex][h+nodey] and self.board[v+nodex][h+nodey]:
+                self.heuristic[v+nodex][h+nodey] = potential
+                self.dist_from_start[v+nodex][h+nodey] = self.dist_from_start[nodex][nodey] + 1
+                self.node_from[v+nodex][h+nodey] = self.probe
+                self.inprogress.append((v+nodex, h+nodey))
+
+
+        self.explored.append(self.probe)
+
+        index = 0
+        for (x,y) in self.inprogress:                
+            if x == self.probe[0] and y == self.probe[1]:
+                self.inprogress.pop(index)
+
+            index += 1   
+        
+
+    def nextStep(self):
+        if self.dest not in self.inprogress:
+            
+            self.selectPromising()
+
+            self.updateNeighbours()
+            return True
+        
+        return False
+    
+    def addOrigin(self, node):
+        self.origin = node
+        self.inprogress.append(node)
+
+    def addToPath(self, list, node):
+        prev_node = self.node_from[node[0]][node[1]]
+        if prev_node != self.origin:
+            list.append(prev_node)
+        return prev_node
+
+    
 
